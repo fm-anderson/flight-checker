e36309753c98b561033c1dee4a8aeb2442c792a7
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2023-08-12 16:26:07 -0400

    fix: consts organized

--- src/js/const.js
+++ src/js/const.js
@@ -1,14 +1,19 @@
-export const invalidText = document.querySelector("#invalid-data");
+export const navLogo = document.querySelector("#navbar-logo");
+export const themeSelector = document.querySelector(".theme-selector");
+
+export const mainContent = document.querySelector("#content");
+
+export const heroDiv = document.querySelector("#hero");
+export const heroImage = document.querySelector("#hero-img");
+
+export const mainForm = document.querySelector("#form");
 export const countryInput = document.querySelector("#input-country");
 export const airportInput = document.querySelector("#input-airport");
-export const mainForm = document.querySelector("#form");
-export const heroDiv = document.querySelector("#hero");
-export const template = document.querySelector("#flights-table");
+export const invalidText = document.querySelector("#invalid-data");
 export const clearButton = document.querySelector("#clear-button");
-export const mainContent = document.querySelector("#content");
-export const themeSelector = document.querySelector(".theme-selector");
-export const heroImage = document.querySelector("#hero-img");
-export const navLogo = document.querySelector("#navbar-logo");
+
+export const template = document.querySelector("#flights-table");
+
 export const resFields = [
   "arr_time",
   "airline_iata",
